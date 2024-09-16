f7c8aab1f5cd2f5d757189f2b7fb4df77b6082c7
켱일 <77844251+KimKyounGs@users.noreply.github.com>
2024-09-16 23:42:14 +0900

    Bullet 스크립트

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -57,17 +57,9 @@ public class Weapon : MonoBehaviour
             // 부모 설정 후 초기 위치를 부모의 위치로 이동
             bullet.parent = transform;
             bullet.position = transform.position; // 부모(중심)의 위치를 기준으로 총알을 배치
-
-            // 각도 계산 (360도를 총알 개수로 나눠서 균등 배치)
             float angle = 360f * i / count;
-
-            // 총알을 Z축을 기준으로 회전
             bullet.Rotate(0, 0, angle);
-
-            // 총알을 현재 로컬 방향에서 뒤로 이동
             bullet.Translate(Vector2.up * -1.5f, Space.Self);
-
-            // 총알 초기화
             bullet.GetComponent<Bullet>().Init(damage, -1); // -1은 무한
         }
     }
